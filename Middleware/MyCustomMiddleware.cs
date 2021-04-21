156346674518cb754ac9d1851acb4fc64fcba636
Patryk Osak <pionchaoo@gmail.com>
2021-04-21 23:18:58 +0200

    Add project files.

---
+++ Middleware/MyCustomMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Shyjus.BrowserDetection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middleware.Middleware
+{
+    public class MyCustomMiddleware
+    {
+        private RequestDelegate next;
+
+        public MyCustomMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext,
+                                      IBrowserDetector detector)
+        {
+            var browser = detector.Browser;
+
+            if (browser.Name == BrowserNames.Edge||browser.Name==BrowserNames.EdgeChromium||browser.Name==BrowserNames.InternetExplorer)
+            {
+                await httpContext.Response
+                      .WriteAsync("Przegladarka nie jest obslugiwana");
+            }
+                await next.Invoke(httpContext);
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Shyjus.BrowserDetection;
+
+namespace Middleware.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IBrowserDetector _browserDetector;
+
+        public IndexModel(ILogger<IndexModel> logger, IBrowserDetector browserDetector)
+        {
+            _logger = logger;
+            _browserDetector = browserDetector;
+        }
+
+        public void OnGet()
+        {
+            var browser = _browserDetector.Browser;
+        }
+    }
+}
